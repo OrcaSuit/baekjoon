24def514214191c6a099c7fc8fed4e4acb942ceb
김건우 <orcasuit@gmail.com>
2023-11-01 19:44:41 +0900

    다시

--- 1181_단어정렬.py
+++ 1181_단어정렬.py
@@ -1,20 +1,22 @@
-testCount = int(input())
-word_list = []
-new_word_list = []
+import sys
 
-#테스트 케이스 입력 리스트에 단어 붙이기
-for _ in range(testCount) :
-    word = input()
-    word_list.append(word)
+count = int(sys.stdin.readline())
+words = []
 
-#중복되는 단어 제거
-for word in word_list:
-    if word not in new_word_list:
-        new_word_list.append(word)
+for _ in range(count):
+    words.append(sys.stdin.readline().strip())
 
-print(new_word_list)
+sorted_words = sorted(words, key=lambda x: (len(x), x)) #람다, 길이반환, 단어자체비교반환
 
-#길이가 짧은 순서로 정렬, 길이가 같을 경우 사전순으로
-#단어의 길이를 비교하고 그 후 길이가 같다면 단어 자체를 사전순으로 비교
-sorted_list = sorted(new_word_list, key=lambda x: (len(x),x)) 
+unique_words = []
+seen = set()
+
+for sorted_word in sorted_words:
+    if sorted_word not in seen:
+        unique_words.append(sorted_word)
+        seen.add(sorted_word)
+
+
+for i in range(len(unique_words)):
+    print(unique_words[i])
 
